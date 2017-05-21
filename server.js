d7841072f086ed6b707fcf83fd86eb446e05508e
Andrew Mundy <andrewmundy@gmail.com>
2017-05-21 13:55:02 -0700

    changed port

--- server.js
+++ server.js
@@ -8,7 +8,5 @@ app.use(bodyParser.urlencoded({ extended: true }));
 MongoClient.connect(db.url, (err, database) => {
   if (err) return console.log(err)
   require('./app/routes')(app, database);
-  app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+  app.listen(process.env.PORT || 5000);
 })
