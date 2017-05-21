e27c42b17f4dc67149ea0b5047360a3cd790ebe9
Andrew Mundy <andrewmundy@gmail.com>
2017-05-21 13:58:28 -0700

    port

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const bodyParser     = require('body-parser');
 const db             = require('./config/db');
 const app            = express();
 
+const port = process.env.PORT || 8000
+
 app.use(bodyParser.urlencoded({ extended: true }));
 MongoClient.connect(db.url, (err, database) => {
   if (err) return console.log(err)
   require('./app/routes')(app, database);
-  app.listen(process.env.PORT || 5000);
+  app.listen(port, function() {
+    console.log("App is running on port " + port);
+});
 })
