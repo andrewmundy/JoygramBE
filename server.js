d0cafaf262df474b3dfd8a34dd43a18f5055e7bc
Andrew Mundy <andrewmundy@gmail.com>
2017-05-21 22:09:22 -0700

    parse

--- server.js
+++ server.js
@@ -1,28 +1,15 @@
-var express = require('express');
-var path = require("path");
-var bodyParser = require("body-parser");
-var mongodb = require("mongodb");
-var ObjectID = mongodb.ObjectID;
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const db             = require('./config/db');
+const app            = express();
+const port = 8000;
 
-var USERS_COLLECTION = "users";
-
-var app = express();
-app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.json());
-
-var db
-
-mongodb.MongoClient.connect("mongodb://andrewmundy:unreal@ds149551.mlab.com:49551/joygram_api", function (err, database) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-  db = database;
-  users = db.collection("users");
-  photos = db.collection("photos");
-  var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port;
-    console.log("App now running on port", port);
+app.use(bodyParser.urlencoded({ extended: true }));
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+  require('./app/routes')(app, database);
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
   });
-});
+})
